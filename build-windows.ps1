e8070cf2189e548c28dae5a442e7fbb8076d6e31
doreso999-crypto <doreso999@gmail.com>
2026-08-28 04:43:05 +0800

    Remove obsolete desktop build script

--- build-windows.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference = 'Stop'
-Write-Host 'Installing dependencies...'
-npm install
-Write-Host 'Building PDF Workspace for Windows...'
-npm run dist:win
-Write-Host 'Done. Check the dist folder.'
