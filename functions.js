5bcd3fe16abc3af11f7770c0510ad72625986527
doreso999-crypto <doreso999@gmail.com>
2026-08-29 03:54:42 +0800

    Load isolated Authorization image drop feature

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
 /* TUBOL PDF Workspace loader.
    Original application remains in functions-core.js.
    Renderer, authorization UI, thumbnail controls, UI consistency, and preview theme load before initialization. */
-document.write('<link rel="stylesheet" href="thumbnail-size.css"><link rel="stylesheet" href="modal-consistency.css"><link rel="stylesheet" href="preview-theme.css"><script src="functions-core.js"><\/script><script src="pdf-render-fix.js"><\/script><script src="authorization-ui.js"><\/script><script src="thumbnail-size-ui.js"><\/script><script src="workspace-ui-fixes.js"><\/script>');
+document.write('<link rel="stylesheet" href="thumbnail-size.css"><link rel="stylesheet" href="modal-consistency.css"><link rel="stylesheet" href="preview-theme.css"><link rel="stylesheet" href="authorization-image-drop.css"><script src="functions-core.js"><\/script><script src="pdf-render-fix.js"><\/script><script src="authorization-ui.js"><\/script><script src="thumbnail-size-ui.js"><\/script><script src="workspace-ui-fixes.js"><\/script><script src="authorization-image-drop.js"><\/script>');
