13a419c3d3852b80037bf72bf9046c54cb368634
doreso999-crypto <doreso999@gmail.com>
2026-08-29 07:42:17 +0800

    Remove thumbnail size trigger from M&O loader

--- functions.js
+++ functions.js
@@ -10,14 +10,12 @@ document.write(`
     }
   </style>
   <link rel="stylesheet" href="responsive-sidebar-hidden.css">
-  <link rel="stylesheet" href="thumbnail-size.css">
   <link rel="stylesheet" href="modal-consistency.css">
   <link rel="stylesheet" href="preview-theme.css">
   <link rel="stylesheet" href="full-content-workspace.css">
   <link rel="stylesheet" href="authorization-mdo.css">
   <script src="functions-core.js"><\/script>
   <script src="pdf-render-fix.js"><\/script>
-  <script src="thumbnail-size-ui.js"><\/script>
   <script src="workspace-ui-fixes.js"><\/script>
   <script src="authorization-mdo.js"><\/script>
 `);
