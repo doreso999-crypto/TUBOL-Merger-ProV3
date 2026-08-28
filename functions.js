00ea5afb05543ee1a676be885ead183de50e2eb5
doreso999-crypto <doreso999@gmail.com>
2026-08-28 21:26:42 +0800

    Load rotation, theme, and modal consistency fixes

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
 /* TUBOL PDF Workspace loader.
    Original application remains in functions-core.js.
-   Renderer, authorization UI, and M&O thumbnail controls load before initialization. */
-document.write('<link rel="stylesheet" href="thumbnail-size.css"><script src="functions-core.js"><\/script><script src="pdf-render-fix.js"><\/script><script src="authorization-ui.js"><\/script><script src="thumbnail-size-ui.js"><\/script>');
+   Renderer, authorization UI, thumbnail controls, and UI consistency fixes load before initialization. */
+document.write('<link rel="stylesheet" href="thumbnail-size.css"><link rel="stylesheet" href="modal-consistency.css"><script src="functions-core.js"><\/script><script src="pdf-render-fix.js"><\/script><script src="authorization-ui.js"><\/script><script src="thumbnail-size-ui.js"><\/script><script src="workspace-ui-fixes.js"><\/script>');
