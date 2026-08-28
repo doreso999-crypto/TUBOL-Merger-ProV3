49eedd71586c3520d6f9532a428c8286d0b79591
doreso999-crypto <doreso999@gmail.com>
2026-08-28 21:15:26 +0800

    Load thumbnail size controls

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
 /* TUBOL PDF Workspace loader.
-   The original application remains in functions-core.js.
-   Renderer and authorization UI fixes load before DOMContentLoaded. */
-document.write('<script src="functions-core.js"><\/script><script src="pdf-render-fix.js"><\/script><script src="authorization-ui.js"><\/script>');
+   Original application remains in functions-core.js.
+   Renderer, authorization UI, and M&O thumbnail controls load before initialization. */
+document.write('<link rel="stylesheet" href="thumbnail-size.css"><script src="functions-core.js"><\/script><script src="pdf-render-fix.js"><\/script><script src="authorization-ui.js"><\/script><script src="thumbnail-size-ui.js"><\/script>');
