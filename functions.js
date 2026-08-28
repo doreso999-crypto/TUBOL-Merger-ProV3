eee680200f3a2dc4d0f2fb484a35c11c26becff0
doreso999-crypto <doreso999@gmail.com>
2026-08-29 06:49:08 +0800

    Restore working M&O Authorization loader

--- functions.js
+++ functions.js
@@ -1,17 +1,11 @@
 /* TUBOL PDF Workspace loader.
    Keep the working M&O Authorization insertion workflow, but remove the
-   Authorization Letter editor surface and all document-editor-only UI/scripts. */
+   Authorization Letter editor surface and all editor-specific enhancement scripts. */
 
 document.write(`
   <style id="tubol-authorization-editor-disabled">
     #letterView,
-    [data-view="letterView"],
-    #saveTemplateModal,
-    #editorSettingsBar,
-    #editorToolbar,
-    #letterEditor,
-    #wordCount,
-    #letterView + * {
+    [data-view="letterView"] {
       display: none !important;
     }
   </style>
@@ -24,20 +18,4 @@ document.write(`
   <script src="authorization-ui.js"><\/script>
   <script src="thumbnail-size-ui.js"><\/script>
   <script src="workspace-ui-fixes.js"><\/script>
-  <script>
-    (() => {
-      const removeAuthorizationEditorSurface = () => {
-        document.querySelector('[data-view="letterView"]')?.remove();
-        document.getElementById('letterView')?.remove();
-        document.getElementById('saveTemplateModal')?.remove();
-        document.getElementById('editorSettingsBar')?.remove();
-        document.getElementById('editorToolbar')?.remove();
-      };
-      if (document.readyState === 'loading') {
-        document.addEventListener('DOMContentLoaded', removeAuthorizationEditorSurface, { once: true });
-      } else {
-        removeAuthorizationEditorSurface();
-      }
-    })();
-  <\/script>
 `);
