cdc9fcd06c6a122241f0bdb08778ad686253cfb9
doreso999-crypto <doreso999@gmail.com>
2026-09-01 00:19:34 +0800

    Prevent stale settings handler from aborting M&O init

--- functions.js
+++ functions.js
@@ -5,6 +5,7 @@ document.write(`
   <link rel="stylesheet" href="full-content-workspace.css">
   <link rel="stylesheet" href="authorization-mdo.css">
   <link rel="stylesheet" href="settings.css">
+  <script>window.openSettings = window.openSettings || function(){};<\/script>
   <script src="functions-core.js"><\/script>
   <script src="pdf-render-fix.js"><\/script>
   <script src="workspace-ui-fixes.js"><\/script>
