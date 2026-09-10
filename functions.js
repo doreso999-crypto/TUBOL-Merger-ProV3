8e97b495bcd45c9a4634d31babd47196011cc102
doreso999-crypto <doreso999@gmail.com>
2026-09-10 23:39:51 +0800

    Integrate converter actions into packet workspace

--- functions.js
+++ functions.js
@@ -1,15 +1,16 @@
-/* TUBOL PDF Workspace loader. Core workspace, Authorization, and Settings modules. */
+/* TUBOL PDF Workspace loader. Core workspace, Authorization, Settings, and conversion modules. */
 document.write(`
   <link rel="stylesheet" href="modal-consistency.css">
   <link rel="stylesheet" href="preview-theme.css">
   <link rel="stylesheet" href="full-content-workspace.css">
   <link rel="stylesheet" href="authorization-mdo.css">
   <link rel="stylesheet" href="settings.css">
-  <script>window.openSettings = window.openSettings || function(){};<\/script>
-  <script src="functions-core.js"><\/script>
-  <script src="pdf-render-fix.js"><\/script>
-  <script src="workspace-ui-fixes.js"><\/script>
-  <script src="authorization-mdo.js"><\/script>
-  <script src="settings.js"><\/script>
-  <script src="converter.js"><\/script>
+  <script>window.openSettings = window.openSettings || function(){};<\\/script>
+  <script src="functions-core.js"><\\/script>
+  <script src="pdf-render-fix.js"><\\/script>
+  <script src="workspace-ui-fixes.js"><\\/script>
+  <script src="authorization-mdo.js"><\\/script>
+  <script src="settings.js"><\\/script>
+  <script src="converter.js"><\\/script>
+  <script src="converter-ui-integration.js"><\\/script>
 `);
